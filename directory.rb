a23d91cd2ad005b4d9168f9743627d1d8d443a7f
mateuszstacel <mateusz.stacel92@gmail.com>
2018-10-22 18:26:00 +0000

    added method print_specific_names

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   # return the array of students
   students
@@ -33,8 +33,22 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+
+def print_specific_names(students)
+  counter = 1
+  puts "please type in specific letter"
+  letter = gets.chomp.capitalize
+  students.each do |student|
+    if student[:name].start_with?(letter)
+      puts "#{counter}. #{student[:name]} (#{student[:cohort]} cohort)"
+      counter += 1
+    end
+  end
+end
+
 #nothing happens until we call the methods
 students = input_students
 print_header
 print(students)
 print_footer(students)
+print_specific_names(students)
